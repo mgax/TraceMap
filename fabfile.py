5e9e02456c82a5ebd66902d2979ebdb6cbf2695a
Alex Morega <alex@grep.ro>
2011-06-30 23:43:07 +0300

    fabric configuration file

---
+++ fabfile.py
@@ -0,0 +1,41 @@
+from fabric.api import env, local, cd, run, put
+from fabric.contrib.files import exists
+from local_fabfile import *
+
+server_prefix = "/home/alexm/sites/maps.grep.ro"
+server_repo = "%s/src/TraceMap" % server_prefix
+server_www = "%s/www" % server_prefix
+server_tiles = "%s/tiles" % server_prefix
+
+
+def _push_code():
+    local("git push -f 'redcoat:%s' HEAD:incoming" % server_repo)
+
+def install_server():
+    run("mkdir -p '%s'" % server_prefix)
+
+    run("mkdir -p '%s'" % server_www)
+    with cd(server_www):
+        run("ln -s ../tiles")
+        run("ln -s ../src/TraceMap/no-data.png")
+
+    if not exists(server_repo):
+        run("mkdir -p '%s'" % server_repo)
+        with cd(server_repo):
+            run("git init")
+        _push_code()
+        with cd(server_repo):
+            run("git checkout incoming -b deploy")
+
+    #with cd(server_prefix):
+    #    run("virtualenv -p /usr/bin/python --distribute .")
+
+def push():
+    _push_code()
+    with cd(server_repo):
+        run("git reset incoming --hard")
+
+def upload():
+    from fabric.contrib.project import rsync_project
+    run("mkdir -p '%s'" % server_tiles)
+    rsync_project(server_tiles, "data/tiles/", delete=True)
